90a7d51d9a9172495a2b9063141e38a7258dec61
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2025-08-04 18:15:04 +0200

    feat(database): add reels table and transaction helpers for reels

--- src/core/database/database.transactions.ts
+++ src/core/database/database.transactions.ts
@@ -1,14 +1,23 @@
 import type { Database } from "better-sqlite3"
-import { CreatePostDto } from "src/modules/posts/posts.types"
+import { CreatePostDto } from "src/modules/posts"
+import { CreateReelDto } from "src/modules/reels"
 
 // This factory function creates and returns our transaction helpers.
 const createTransactionHelpers = (db: Database) => {
     // We use prepared statements for security and performance.
     const statements = {
+        // POSTS
         getPostById: db.prepare("SELECT * FROM posts WHERE id = ?"),
         getAllPosts: db.prepare("SELECT * FROM posts"),
         createPost: db.prepare(
             "INSERT INTO posts (img_url, caption) VALUES (@img_url, @caption) RETURNING *"
+        ),
+
+        // REELS
+        getReelById: db.prepare("SELECT * FROM reels WHERE id = ?"),
+        getAllReels: db.prepare("SELECT * FROM reels"),
+        createReel: db.prepare(
+            "INSERT INTO reels (video_url, thumbnail_url, caption) VALUES (@video_url, @thumbnail_url, @caption) RETURNING *"
         ),
     }
 
@@ -24,8 +33,21 @@ const createTransactionHelpers = (db: Database) => {
         },
     }
 
+    const reels = {
+        getById: (id: number) => {
+            return statements.getReelById.get(id)
+        },
+        getAll: () => {
+            return statements.getAllReels.all()
+        },
+        create: (data: CreateReelDto) => {
+            return statements.createReel.get(data)
+        },
+    }
+
     return {
         posts,
+        reels,
     }
 }
 
