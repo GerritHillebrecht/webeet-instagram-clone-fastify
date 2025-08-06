7f2460971117c6625fdd5c43a35dc4d121b32681
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2025-08-06 17:24:02 +0200

    feat(server): configure Cloudinary and register multipart and static plugins

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,34 @@
 import Fastify from "fastify"
 import { databasePlugin } from "src/core/database"
 import { postsRoutes, reelsRoutes, taggedRoutes } from "./modules"
-import { storiesRoutes } from "./modules/stories/stories.routes"
 import { highlightRoutes } from "./modules/highlights"
+import { storiesRoutes } from "./modules/stories/stories.routes"
+import path from "path"
+
+import multipart from "@fastify/multipart"
+import fastifyStatic from "@fastify/static"
+import { v2 as cloudinary } from "cloudinary"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
 const fastify = Fastify({
     logger: true,
+})
+
+fastify.register(fastifyStatic, {
+    root: path.join(__dirname, "..", "public"),
+    prefix: "/", // oder z. B. '/static/' falls du nur unter dieser Route zugreifen willst
+})
+
+// Register multipart plugin
+fastify.register(multipart, {
+    limits: {
+        fileSize: 5 * 1024 * 1024, // 5 MB
+    },
 })
 
 // Register our database plugin
